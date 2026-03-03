99f35b7bd9669f33e43ebfad316787702218bbe3
Kirll Savhcenko <Savhcenko55533@gmail.com>
2026-03-03 21:19:27 +0300

    adds new router customers

--- backend/app/api_v1/__init__.py
+++ backend/app/api_v1/__init__.py
@@ -1,11 +1,16 @@
 
 from fastapi import APIRouter
 
-from .users.views import router as user_roter
-
+from .users.views import router as user_router
+from .customers.views import router as customer_router
 router = APIRouter()
 
 router.include_router(
-    router=user_roter,
+    router=user_router,
     prefix="/user"
+)
+
+router.include_router(
+    router=customer_router,
+    prefix="/customer"
 )
